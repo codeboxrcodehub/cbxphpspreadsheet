47f0141ebd2555da781d18922a5215e450831ce8
Deepak3642 <deepakdeb001@gmail.com>
2025-04-15 15:12:58 +0600

    updater process updated, textdomain hook call on init

--- cbxphpspreadsheet.php
+++ cbxphpspreadsheet.php
@@ -46,12 +46,19 @@ register_activation_hook( __FILE__, [ 'CBXPhpSpreadSheet', 'activation' ] );
  */
 class CBXPhpSpreadSheet {
 	public function __construct() {
+		
 		// Load text domain
-		load_plugin_textdomain( 'cbxphpspreadsheet', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-
+		add_action( 'init', [ $this, 'load_plugin_textdomain' ]);
+		
 		// Add custom row meta links
 		add_filter( 'plugin_row_meta', [ $this, 'plugin_row_meta' ], 10, 2 );
 		add_action( 'admin_notices', [ $this, 'activation_error_display' ] );
+
+		add_filter( 'pre_set_site_transient_update_plugins', [
+			$this,
+			'pre_set_site_transient_update_plugins'
+		] );
+		add_filter( 'plugins_api', [ $this, 'plugin_info' ], 10, 3 );
 	}//end constructor
 
 	/**
@@ -158,6 +165,116 @@ class CBXPhpSpreadSheet {
 
 		return $links;
 	}
+
+	/**
+	 * Load textdomain
+	 *
+	 * @since 1.0.0
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain( 'cbxphpspreadsheet', false, CBXPHPSPREADSHEET_ROOT_PATH . 'languages/' );
+	}//end method load_plugin_textdomain
+
+	/**
+	 * Custom update checker implemented
+	 *
+	 * @param $transient
+	 *
+	 * @return mixed
+	 */
+	public function pre_set_site_transient_update_plugins( $transient ) {
+		// Ensure the transient is set
+		if ( empty( $transient->checked ) ) {
+			return $transient;
+		}
+
+		$plugin_slug = 'cbxphpspreadsheet';
+		$plugin_file = 'cbxphpspreadsheet/cbxphpspreadsheet.php';
+
+		if ( isset( $transient->response[ $plugin_file ] ) ) {
+			return $transient;
+		}
+
+		if ( ! function_exists( 'get_plugins' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+
+		$url = 'https://comforthrm.com/product_updates.json'; // Replace with your remote JSON file URL
+		
+		// Fetch the remote JSON file
+		$response = wp_remote_get( $url );
+
+		if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != 200 ) {
+			return $transient;
+		}
+
+		$data = json_decode( wp_remote_retrieve_body( $response ), true );// Set true for associative array, false for object
+
+
+		if ( ! isset( $data['cbxphpspreadsheet'] ) ) {
+			return $transient;
+		}
+
+		$remote_data = $data['cbxphpspreadsheet'];
+
+		$plugin_url  = isset( $remote_data['url'] ) ? $remote_data['url'] : '';
+		$package_url = isset( $remote_data['api_url'] ) ? $remote_data['api_url'] : false;
+
+		$remote_version = isset( $remote_data['new_version'] ) ? sanitize_text_field( $remote_data['new_version'] ) : '';
+
+		if ( $remote_version != '' && version_compare( $remote_version, $transient->checked[ $plugin_file ], '>' ) ) {
+			$transient->response[ $plugin_file ] = (object) [
+				'slug'        => $plugin_slug,
+				'new_version' => $remote_version,
+				'url'         => $plugin_url,
+				'package'     => $package_url, // Link to the new version
+			];
+		}
+
+		return $transient;
+	}//end method pre_set_site_transient_update_plugins
+
+	public function plugin_info( $res, $action, $args ) {
+		// Plugin slug
+		$plugin_slug = 'cbxphpspreadsheet';                                      // Replace with your plugin slug
+
+		// Ensure we're checking the correct plugin
+		if ( $action !== 'plugin_information' || $args->slug !== $plugin_slug ) {
+			return $res;
+		}
+
+		// Fetch detailed plugin information
+		$response = wp_remote_get( 'https://comforthrm.com/product_updates.json' ); // Replace with your API URL
+
+		if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != 200 ) {
+			return $res;
+		}
+
+		$data = json_decode( wp_remote_retrieve_body( $response ), true );
+		if ( ! isset( $data[ $plugin_slug ] ) ) {
+			return $res;
+		}
+
+		$remote_data = $data[ $plugin_slug ];		
+		$package_url = isset( $remote_data['api_url'] ) ? $remote_data['api_url'] : false;
+
+		// Build the plugin info response
+		return (object) [
+			'name'          => isset( $remote_data['name'] ) ? sanitize_text_field( $remote_data['name'] ) : 'CBX PhpSpreadSheet Library',
+			'slug'          => $plugin_slug,
+			'version'       => isset( $remote_data['new_version'] ) ? sanitize_text_field( $remote_data['new_version'] ) : '',
+			'author'        => isset( $remote_data['author'] ) ? sanitize_text_field( $remote_data['author'] ) : '',
+			'homepage'      => isset( $remote_data['url'] ) ? $remote_data['url'] : '',
+			'requires'      => isset( $remote_data['requires'] ) ? sanitize_text_field( $remote_data['requires'] ) : '',
+			'tested'        => isset( $remote_data['tested'] ) ? sanitize_text_field( $remote_data['tested'] ) : '',
+			'download_link' => $package_url,
+			'sections'      => [
+				'description' => isset( $remote_data['description'] ) ? wp_kses_post( $remote_data['description'] ) : '',
+				'changelog'   => isset( $remote_data['changelog'] ) ? wp_kses_post( $remote_data['changelog'] ) : '',
+			],
+		];
+
+	}//end method plugin_info
 }//end class CBXPhpSpreadSheet
 
 /**
